f6cc42a688d629fa4e8e1b5ecaf3cc6e1a953603
Utsav Singal <62511146+usingal04@users.noreply.github.com>
2026-09-21 19:10:43 -0600

    Add: asteroid-collision - submission-3

---
+++ Data Structures & Algorithms/asteroid-collision/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        
+        stk = []
+
+        for a in asteroids:
+            while stk and a < 0 and stk[-1] > 0:
+                diff = a + stk[-1]
+
+                if diff < 0:
+                    stk.pop()
+                elif diff > 0:
+                    a = 0
+                else:
+                    stk.pop()
+                    a = 0
+            if a:
+                stk.append(a)
+    
+        return stk
