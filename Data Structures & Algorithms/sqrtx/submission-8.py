f6a5fa8c16ca07b06138c5d159aa6e87d69de42f
Utsav Singal <62511146+usingal04@users.noreply.github.com>
2026-09-22 19:34:47 -0600

    Add: sqrtx - submission-8

---
+++ Data Structures & Algorithms/sqrtx/submission-8.py
@@ -0,0 +1,18 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        
+        l, r = 1, x
+        res = 0
+
+        while l <= r:
+            mid = l + (r-l) // 2
+            if mid * mid == x:
+                res = mid
+                return res
+            elif mid * mid > x:
+                r = mid-1
+            else:
+                l = mid+1
+                res = mid
+        
+        return res
